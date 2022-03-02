f044004013322efa40974feaba76914cff7405aa
Rayato159 <rayato159@gmail.com>
2022-03-02 14:35:07 +0700

    :sparkles: update services are completed.

--- src/services/tableServices.js
+++ src/services/tableServices.js
@@ -74,6 +74,22 @@ export const createTable1 = (eye_photo_id, yes, cannot_grade) => {
     })
 }
 
+export const updateTable1 = (eye_photo_id, yes, cannot_grade) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const res = await table1Controller.patch(`${eye_photo_id}/update`,{
+                yes,
+                cannot_grade
+            },{
+                headers: accessToken
+            })
+            resolve(res.data)
+        } catch(e) {
+            reject(e.response.data)
+        }
+    })
+}
+
 // Table 2
 export const createTable2 = (eye_photo_id, yes, cannot_grade) => {
     return new Promise(async (resolve, reject) => {
@@ -91,11 +107,43 @@ export const createTable2 = (eye_photo_id, yes, cannot_grade) => {
     })
 }
 
+export const updateTable2 = (eye_photo_id, yes, cannot_grade) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const res = await table2Controller.patch(`${eye_photo_id}/update`,{
+                yes,
+                cannot_grade
+            },{
+                headers: accessToken
+            })
+            resolve(res.data)
+        } catch(e) {
+            reject(e.response.data)
+        }
+    })
+}
+
 // Table 3
 export const createTable3 = (eye_photo_id, yes, cannot_grade) => {
     return new Promise(async (resolve, reject) => {
         try {
             const res = await table3Controller.post(`${eye_photo_id}/create`,{
+                yes,
+                cannot_grade
+            },{
+                headers: accessToken
+            })
+            resolve(res.data)
+        } catch(e) {
+            reject(e.response.data)
+        }
+    })
+}
+
+export const updateTable3 = (eye_photo_id, yes, cannot_grade) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const res = await table3Controller.patch(`${eye_photo_id}/update`,{
                 yes,
                 cannot_grade
             },{
@@ -126,11 +174,44 @@ export const createTable4 = (eye_photo_id, lower_2a, upper_2a, cannot_grade) => 
     })
 }
 
+export const updateTable4 = (eye_photo_id, lower_2a, upper_2a, cannot_grade) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const res = await table4Controller.patch(`${eye_photo_id}/update`,{
+                lower_2a,
+                upper_2a,
+                cannot_grade
+            },{
+                headers: accessToken
+            })
+            resolve(res.data)
+        } catch(e) {
+            reject(e.response.data)
+        }
+    })
+}
+
 // Table 5
 export const createTable5 = (eye_photo_id, yes, cannot_grade) => {
     return new Promise(async (resolve, reject) => {
         try {
             const res = await table5Controller.post(`${eye_photo_id}/create`,{
+                yes,
+                cannot_grade
+            },{
+                headers: accessToken
+            })
+            resolve(res.data)
+        } catch(e) {
+            reject(e.response.data)
+        }
+    })
+}
+
+export const updateTable5 = (eye_photo_id, yes, cannot_grade) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const res = await table5Controller.patch(`${eye_photo_id}/update`,{
                 yes,
                 cannot_grade
             },{
@@ -161,11 +242,44 @@ export const createTable6 = (eye_photo_id, lower_8a, upper_8a, cannot_grade) => 
     })
 }
 
+export const updateTable6 = (eye_photo_id, lower_8a, upper_8a, cannot_grade) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const res = await table6Controller.patch(`${eye_photo_id}/update`,{
+                lower_8a,
+                upper_8a,
+                cannot_grade
+            },{
+                headers: accessToken
+            })
+            resolve(res.data)
+        } catch(e) {
+            reject(e.response.data)
+        }
+    })
+}
+
 // Table 7
 export const createTable7 = (eye_photo_id, yes, cannot_grade) => {
     return new Promise(async (resolve, reject) => {
         try {
             const res = await table7Controller.post(`${eye_photo_id}/create`,{
+                yes,
+                cannot_grade
+            },{
+                headers: accessToken
+            })
+            resolve(res.data)
+        } catch(e) {
+            reject(e.response.data)
+        }
+    })
+}
+
+export const updateTable7 = (eye_photo_id, yes, cannot_grade) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const res = await table7Controller.patch(`${eye_photo_id}/update`,{
                 yes,
                 cannot_grade
             },{
@@ -195,11 +309,43 @@ export const createTable8 = (eye_photo_id, yes, cannot_grade) => {
     })
 }
 
+export const updateTable8 = (eye_photo_id, yes, cannot_grade) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const res = await table8Controller.patch(`${eye_photo_id}/update`,{
+                yes,
+                cannot_grade
+            },{
+                headers: accessToken
+            })
+            resolve(res.data)
+        } catch(e) {
+            reject(e.response.data)
+        }
+    })
+}
+
 // Table 9
 export const createTable9 = (eye_photo_id, yes, cannot_grade) => {
     return new Promise(async (resolve, reject) => {
         try {
             const res = await table9Controller.post(`${eye_photo_id}/create`,{
+                yes,
+                cannot_grade
+            },{
+                headers: accessToken
+            })
+            resolve(res.data)
+        } catch(e) {
+            reject(e.response.data)
+        }
+    })
+}
+
+export const updateTable9 = (eye_photo_id, yes, cannot_grade) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const res = await table9Controller.patch(`${eye_photo_id}/update`,{
                 yes,
                 cannot_grade
             },{
@@ -230,6 +376,22 @@ export const createTable10 = (eye_photo_id, yes, cannot_grade) => {
     })
 }
 
+export const updateTable10 = (eye_photo_id, yes, cannot_grade) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const res = await table10Controller.patch(`${eye_photo_id}/update`,{
+                yes,
+                cannot_grade
+            },{
+                headers: accessToken
+            })
+            resolve(res.data)
+        } catch(e) {
+            reject(e.response.data)
+        }
+    })
+}
+
 // Table 11
 export const createTable11 = (eye_photo_id, yes, cannot_grade) => {
     return new Promise(async (resolve, reject) => {
@@ -247,11 +409,44 @@ export const createTable11 = (eye_photo_id, yes, cannot_grade) => {
     })
 }
 
+export const updateTable11 = (eye_photo_id, yes, cannot_grade) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const res = await table11Controller.patch(`${eye_photo_id}/update`,{
+                yes,
+                cannot_grade
+            },{
+                headers: accessToken
+            })
+            resolve(res.data)
+        } catch(e) {
+            reject(e.response.data)
+        }
+    })
+}
+
 // Table 12
 export const createTable12 = (eye_photo_id, lower_2DD, lower_1DD, cannot_grade) => {
     return new Promise(async (resolve, reject) => {
         try {
             const res = await table12Controller.post(`${eye_photo_id}/create`,{
+                lower_2DD,
+                lower_1DD,
+                cannot_grade
+            },{
+                headers: accessToken
+            })
+            resolve(res.data)
+        } catch(e) {
+            reject(e.response.data)
+        }
+    })
+}
+
+export const updateTable12 = (eye_photo_id, lower_2DD, lower_1DD, cannot_grade) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const res = await table12Controller.patch(`${eye_photo_id}/update`,{
                 lower_2DD,
                 lower_1DD,
                 cannot_grade
@@ -307,6 +502,57 @@ export const createTable13 = (
     return new Promise(async (resolve, reject) => {
         try {
             const res = await table13Controller.post(`${eye_photo_id}/create`,
+                items,
+                { headers: accessToken },
+            )
+            resolve(res.data)
+        } catch(e) {
+            reject(e.response.data)
+        }
+    })
+}
+
+export const updateTable13 = (
+    eye_photo_id,
+    number,
+) => {
+    // Varible 1 - 5
+    let cataract = false
+    let glaucoma = false
+    let occlusion = false
+    let maculopathy = false
+    let other = false
+
+    // Check which one is true!!!
+    switch(number) {
+        case 1:
+            cataract = true
+            break
+        case 2:
+            glaucoma = true
+            break
+        case 3:
+            occlusion = true
+            break
+        case 4:
+            maculopathy = true
+            break;
+        case 5:
+            other = true
+            break
+    }
+
+    let items = {
+        cataract,
+        glaucoma,
+        occlusion,
+        maculopathy,
+        other
+    }
+
+    return new Promise(async (resolve, reject) => {
+        try {
+            const res = await table13Controller.patch(`${eye_photo_id}/update`,
                 items,
                 { headers: accessToken },
             )
